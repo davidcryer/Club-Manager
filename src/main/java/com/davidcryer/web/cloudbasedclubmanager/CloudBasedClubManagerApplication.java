18533e0868a64ee3ee3bd5a738264f27a0a30efe
David Cryer <davecryer.dc@gmail.com>
2017-09-13 15:07:45 +0100

    Initial project

---
+++ src/main/java/com/davidcryer/web/cloudbasedclubmanager/CloudBasedClubManagerApplication.java
@@ -0,0 +1,12 @@
+package com.davidcryer.web.cloudbasedclubmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBasedClubManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBasedClubManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/davidcryer/web/cloudbasedclubmanager/CloudBasedClubManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.davidcryer.web.cloudbasedclubmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudBasedClubManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
